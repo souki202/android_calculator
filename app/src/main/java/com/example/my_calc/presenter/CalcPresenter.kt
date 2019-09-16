fd9dd6fe62cfebabb3e602d9b227b44533683f81
souki202 <290livermorium@gmail.com>
2019-09-16 15:52:48 +0900

    数字キー, ドット, 四則演算キーを押すと文字が増えるところまで

--- app/src/main/java/com/example/my_calc/presenter/CalcPresenter.kt
+++ app/src/main/java/com/example/my_calc/presenter/CalcPresenter.kt
@@ -1,5 +1,19 @@
 package com.example.my_calc.presenter
 
 class CalcPresenter {
+    private var expression = ""
 
+    constructor() {}
+
+    fun addExpressionChar(char: CharSequence) {
+        expression += char
+    }
+
+    fun getExpression(): String {
+        return expression
+    }
+
+    fun getAnswer() {
+
+    }
 }
--- app/src/main/java/com/example/my_calc/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/my_calc/ui/main/MainFragment.kt
@@ -6,9 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.example.my_calc.R
+import com.example.my_calc.presenter.CalcPresenter
+import org.w3c.dom.Text
 
 class MainFragment : Fragment() {
+
+    private val presenter = CalcPresenter()
+
+    private var numberAndOpOnClickListener = {view: View ->
+        if (view is Button) {
+            presenter.addExpressionChar(view.text)
+            this@MainFragment.view!!.findViewById<TextView>(R.id.expressionInputArea).text = presenter.getExpression()
+            this@MainFragment.view!!.findViewById<TextView>(R.id.expressionOverview).text = presenter.getExpression()
+        }
+    }
 
     companion object {
         fun newInstance() = MainFragment()
@@ -26,6 +40,23 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        // キー割当
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPad0).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPad1).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPad2).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPad3).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPad4).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPad5).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPad6).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPad7).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPad8).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPad9).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPadDot).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPadAdd).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPadSub).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPadMul).setOnClickListener(numberAndOpOnClickListener)
+        this@MainFragment.view!!.findViewById<TextView>(R.id.numPadDiv).setOnClickListener(numberAndOpOnClickListener)
     }
 
 
